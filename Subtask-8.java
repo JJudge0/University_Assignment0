145f23832dd4c03242390aec095d651fc0a7dbd5
JJudge0 <73240114+JJudge0@users.noreply.github.com>
2021-12-14 01:48:19 +0000

    Create Subtask-8.java

---
+++ Subtask-8.java
@@ -0,0 +1,16 @@
+	public static void Conversion_Hexadecimal_to_Decimal()
+	{   
+		System.out.println("Conversion from Hexadecimal to Denary"); 
+		System.out.println("Enter a Hexadecimal number: ");
+		Scanner input2 = new Scanner( System.in );
+        String Hexa=input2.nextLine();     
+        input2.close();
+		int Decimal=Integer.parseInt(Hexa,16);// Takes user input and passes through as Integer then runs it against the base 16 number system.
+		Hexa = Hexa.toUpperCase();  //Puts the input as Upper case
+		System.out.println("HexaDecimal Number Entered:"); 
+		System.out.println(Hexa);  // Prints the users input as Capital Letter
+		System.out.println("Hexadecimal to Denary Conversion is: ");
+		System.out.println(Decimal);
+		System.out.println("-----------------------");
+	}
+	
